028bba37e18c2190e47eb2a232383ab932d88fa7
Coder731 <stephenobrien1@gmail.com>
2023-03-14 06:23:34 +0100

    Trace classes Student and User through files
    Run migrations
    
    Update Untitled Jupyter notebook
    with
    imports
    to output
    all
    Student objects

--- app/views.py
+++ app/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Student, User
 
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    obj1 = Student.objects.all()
+    obj2 = User.objects.all()
+    context = {
+        "obj1": obj1,
+        "obj2": obj2,
+    }
+    return render(request, "index.html", context)
